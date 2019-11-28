d2472f6da5185fe3be239fe2eefcabaee0000614
Salman Janvekar <sjanveka@uwo.ca>
2019-11-27 23:18:08 -0500

    Front-End development started, pages created. Front-end: 10% , Back-end: 85%.

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {HomeComponent} from './home/home.component';
 import {RegistrationComponent} from './registration/registration.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'Home', component: HomeComponent},
-  {path: 'Registration', component: RegistrationComponent}
+  {path: 'Registration', component: RegistrationComponent},
+  {path: 'LogIn', component: LoginComponent},
 
 ];
 
