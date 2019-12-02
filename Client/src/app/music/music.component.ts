75a7022b04ed8cf9ebc9525bbd2e184747e6901e
Salman Janvekar <sjanveka@uwo.ca>
2019-12-02 16:24:37 -0500

    Gets all songs and displays them when the music directory is opened

--- Client/src/app/music/music.component.ts
+++ Client/src/app/music/music.component.ts
@@ -3,6 +3,7 @@ import {NgForm} from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatTableDataSource} from '@angular/material';
 import { HttpClient } from '@angular/common/http';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-music',
@@ -16,24 +17,27 @@ export class MusicComponent implements OnInit {
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-
+    this.song = [];
+    this.fetchSongs();
   }
 
   fetchSongs() {
-
     var uri = 'http://localhost:1234';
-
+    
     //routes to server to get all songs
     this.http.get(uri +'/Music/music').subscribe((data) => {
-      Object.values(data).forEach(item => {
-        this.song.push({
-          Title: item.Title,
-          Artist: item.Artist,
-          Album: item.Album,
-          Year: item.Year,
-          Genre: item.Genre,
-          Rating: item.Rating,
+      Object.values(data).forEach(element => {
+        Object.values(element).forEach(item => {
+          this.song.push({
+            Title: item["Title"],
+            Artist: item["Artist"],
+            Album: item["Album"],
+            Year: item["Year"],
+            Genre: item["Genre"],
+            Rating: item["Rating"],
+          })
         })
+        console.log(this.song);
       });
     })
   }
